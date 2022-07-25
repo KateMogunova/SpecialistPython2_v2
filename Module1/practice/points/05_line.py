19eba6639319e8f46c2ddd3f3a0d332d875c421e
Kate <mogunovakate@gmail.com>
2022-07-25 15:42:42 +0300

    Update 05_line.py
    Получается ноль, но не совсем понимаю. Возможно, все результаты нужно было запихивать в список и потом уже в списке делать +

--- Module1/practice/points/05_line.py
+++ Module1/practice/points/05_line.py
@@ -1,12 +1,23 @@
+from math import sqrt
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def dist_to(self, other_point):
+        return sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
 
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
+
+total = 0
+
 # TODO: Найдите длину ломаной линии
 
-print("Длина ломаной линии = ", ...)
+for point in points:
+    total += point.dist_to(point)
+
+print("Длина ломаной линии = ", total)
